9cc1432143431641d903ff47ab073a218fe5ec33
Benjamin Alan Murphy-Kane <b3nmurphy924@gmail.com>
2022-04-05 20:21:30 -0700

    commit tree eebb5035415e1219a78edb9582590884c4493041

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callbackFunction) {
+    return callbackFunction();
+};
+
+function returnsANamedFunction() {
+    return function namedFunction() {
+
+    }
+};
+
+function returnsAnAnonymousFunction() {
+    return function() {
+
+    }
+    };
